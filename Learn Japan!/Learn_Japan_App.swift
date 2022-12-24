e09ff870acd37100ed928f4050db8f4de418d5d0
TSanni <tomas.sanni@gmail.com>
2022-12-24 01:54:54 -0600

    Initial Commit

---
+++ Learn Japan!/Learn_Japan_App.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_Japan_App.swift
+//  Learn Japan!
+//
+//  Created by Tomas Sanni on 12/24/22.
+//
+
+import SwiftUI
+
+@main
+struct Learn_Japan_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
